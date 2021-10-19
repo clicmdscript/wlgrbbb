2c5e345f9fbd96a217d6a9fe779824f6593f8ff1
Pytorch Project Test 1 <79557662+2Pytorch01@users.noreply.github.com>
2021-10-19 22:33:49 +0700

    Update 4install_scalelite.sh

--- sample/4install_scalelite.sh
+++ sample/4install_scalelite.sh
@@ -187,3 +187,16 @@ echo "Should showing like"
 echo "scalelite-spool:x:2000:bigbluebutton"
 echo "Check with DF -h"
 
+
+
+
+cd /usr/local/bigbluebutton/core/scripts
+rm -rf scalelite.yml
+wget https://raw.githubusercontent.com/blindsidenetworks/scalelite/master/bigbluebutton/scalelite.yml
+cd /usr/local/bigbluebutton/core/scripts
+wget https://raw.githubusercontent.com/blindsidenetworks/scalelite/master/bigbluebutton/scalelite_batch_import.sh
+chmod +x scalelite_batch_import.sh
+./scalelite_batch_import.sh
+
+
+
