692ecaefa4faad230df900c13002872fd9524dca
Pytorch Project Test 1 <79557662+2Pytorch01@users.noreply.github.com>
2021-10-20 09:24:50 +0700

    Update 3install_nfs.sh

--- sample/3install_nfs.sh
+++ sample/3install_nfs.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-# file quan trong: vi /etc/exports
+# Cấu hình tương tự cho Scalelite server
+# File Config server /etc/exports
 # de mount dc tu server client, phai co domain trong file exports tren nfs server
+# File config /etc/exports
+# /etc/fstab: Để tự động mount một thư mục NFS trên hệ thống.
+# cấu hình NFS server /etc/export để xác định các máy client nào có thể truy cập được file được chia sẽ trên NFS server bằng trình soạn thảo vi.
+# Cú pháp như sau:
+# /mnt/sharedfolder client1IP(rw,sync,no_root_squash,no_subtree_check)
+# /mnt/sharedfolder client2IP(rw,sync,no_root_squash,no_subtree_check)
+# sudo exportfs -a
+# Finally, in order to make all the configurations take effect, restart the NFS Kernel server as follows:
+# sudo systemctl restart nfs-kernel-server
+# sudo ufw allow from 192.168.100/24 to any port nfs
+
+
+################################
+# sudo apt-get update
+# sudo apt-get install nfs-common
+######## Mount from client
+######## sudo mkdir -p /mnt/sharedfolder_client
+######## sudo mount 192.168.100.5:/mnt/sharedfolder /mnt/sharedfolder_client
+######## If reboot lost, add to etc/fstab
+######## file config: /etc/fstab
+######## 192.168.1.22:/share      /access  nfs      defaults      0      0
+# umount /access
+# mount /access
+# mount | tail -1
+# 192.168.1.22:/share on /access type nfs (rw,addr=192.168.1.22)
+
 sudo apt-get update
-apt-get install -y nfs-server 
+apt-get install -y nfs-server
 apt-get install -y nfs-kernel-server
 
 echo "/mnt/scalelite-recordings    *(rw,sync,no_root_squash)" > /etc/exports
@@ -13,10 +40,16 @@ sudo /etc/init.d/nfs-kernel-server restart
 #read -p "Enter domain of scalelite server: " dmnamescal
 #echo "Data received"
 
+
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
+
+
 echo "Manual add in /etc/exports"
 echo "/mnt/scalelite-recordings \" 
 echo "domainscale.dm.com(rw,sync,no_root_squash,no_subtree_check) "
 
+echo "Check with command: exportfs"
 echo "Create DIR for /mnt/scalelite-recordings"
 sudo mkdir -p /mnt/scalelite-recordings
 
@@ -25,13 +58,11 @@ sudo /etc/init.d/nfs-kernel-server restart
 
 echo "Start Firewall add domain and IP"
 
-sudo apt update
-sudo apt install -y nfs-kernel-server nfs-common
+echo "ufw allow from IP4HERE"
+# sudo ufw allow from 192.168.100/24 to any port nfs
+# sudo ufw allow from 192.168.100/24 to any port nfs
+# sudo ufw allow from 192.168.100/24 to any port nfs
 
-read -p "Enter IPv4 of scalelite server: " ip4scalelite
-echo "Data received"
-
-ufw allow from $ip4scalelite
 sudo ufw allow 22
 echo "sudo ufw enable"
 echo "sudo ufw reload"
