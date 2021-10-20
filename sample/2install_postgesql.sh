6462ac2420190a1f9f884ac10c6b5685fdddc492
Pytorch Project Test 1 <79557662+2Pytorch01@users.noreply.github.com>
2021-10-20 16:49:33 +0700

    Update 2install_postgesql.sh

--- sample/2install_postgesql.sh
+++ sample/2install_postgesql.sh
@@ -29,46 +29,13 @@ echo "host postgres scalelite 165.22.107.61/32 trust"
 
 read -p "Press enter to continue"
 
-
-echo "Password for PostgeSQL"
-openssl rand -hex 8
 echo "Save it"
 echo ""
-echo "Open new connect ssh and do command below create SQL"
-echo ""
-echo "Password Generator is"
-openssl rand -hex 8
-echo "sudo -u postgres psql"
-echo "CREATE USER scalelite WITH PASSWORD 'PASS_FROM_HEX8';"
-echo "CREATE DATABASE scalelite;"
-echo "GRANT ALL PRIVILEGES ON DATABASE "scalelite" to scalelite;"
-echo "ALTER ROLE scalelite SUPERUSER;"
-echo "\q to EXIT SQL"
-read -p "Press enter to continue"
-
 
 echo ""
 echo "sudo service postgresql restart"
 sudo service postgresql restart
 echo ""
 echo "Allow IP of scalelite server"
-echo ""
-echo "ufw allow from IP SCALELITE"
-echo "sudo ufw allow 5432"
-echo "sudo ufw allow 22"
-echo "sudo ufw reload"
-
-read -p "Press enter to continue"
-
-
-echo "All done for Postgesql"
-echo "Please allow all IP of system to firewall"
-echo "sudo ufw allow from IP"
-echo "enable firewall with command"
-echo "sudo ufw enable"
-
-
-
-
 
 
