c37ebe542867c15272db833be8e61f57469f0ff4
Pytorch Project Test 1 <79557662+2Pytorch01@users.noreply.github.com>
2021-10-22 13:32:52 +0700

    Create croncheckmount.sh

---
+++ sample/croncheckmount.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+df -h |
+if grep -qs '/mnt/scalelite-recordings' /proc/mounts; then
+        echo "It's mounted."
+else
+        echo "It's not mounted."
+        sudo mount file.roauset.com:/mnt/scalelite-recordings /mnt/scalelite-recordings
+fi
